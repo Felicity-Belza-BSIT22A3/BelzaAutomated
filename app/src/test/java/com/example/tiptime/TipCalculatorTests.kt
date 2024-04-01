153ae63a4ff211e7dd7c8dc30fa687e350860cee
Fel <fhelbelza123@gmail.com>
2024-04-01 14:55:04 +0800

    tiptime

---
+++ app/src/test/java/com/example/tiptime/TipCalculatorTests.kt
@@ -0,0 +1,13 @@
+package com.example.tiptime
+
+class TipCalculatorTests {
+    import org.junit.Test
+
+    class TipCalculatorTests {
+
+        @Test
+        fun calculateTip_20PercentNoRoundup() {
+
+        }
+    }
+}
